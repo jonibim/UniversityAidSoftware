d04287f0271826d63c20b859788c179252a314e2
t0xicdream <jonibimbashi@gmail.com>
2018-12-16 15:25:05 +0100

    Update Source.cpp

--- UniversityAid/UniversityAid/Source.cpp
+++ UniversityAid/UniversityAid/Source.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 int main() {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	fgjfgghj
 =======
 	Student Student2;
 >>>>>>> parent of df7263d... shtova person
+=======
+	fgjfgghj
+>>>>>>> parent of c5f32f5... FIxed Errors
 
 
 
