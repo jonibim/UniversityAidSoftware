f4d837b38e148d98890ac1b867c7296570938290
t0xicdream <jonibimbashi@gmail.com>
2018-12-16 15:29:14 +0100

    Restored Default State

--- UniversityAid/UniversityAid/Source.cpp
+++ UniversityAid/UniversityAid/Source.cpp
@@ -1,28 +1,6 @@
 #include "Student.h"
 using namespace std;
 int main() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	fgjfgghj
-=======
-	Student Student2;
->>>>>>> parent of df7263d... shtova person
-=======
-	fgjfgghj
->>>>>>> parent of c5f32f5... FIxed Errors
-=======
-	fgjfgghj
->>>>>>> parent of c5f32f5... FIxed Errors
-=======
-
->>>>>>> parent of 058955b... test
-=======
-	Student Student2;
->>>>>>> parent of df7263d... shtova person
-
 
 
 }
