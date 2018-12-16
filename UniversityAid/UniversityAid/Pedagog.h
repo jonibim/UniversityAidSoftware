704660904dc3b94a0a710eedf0d74bc6bf209cf1
t0xicdream <jonibimbashi@gmail.com>
2018-12-16 15:37:42 +0100

    Updetova dhe riemerova Branch-in Shtova funksione te tjera ne Pedagog.h

--- UniversityAid/UniversityAid/Pedagog.h
+++ UniversityAid/UniversityAid/Pedagog.h
@@ -1,6 +1,24 @@
 #pragma once
 using namespace std;
-class Pedagog {
+#include "Student.h"
+class Pedagog : public Person {
 
+private:
+		string password;
+
+public:
+	Pedagog() :Person() {}
+	Pedagog(string name, string surname) :Person(name, surname) {}
+	string showName() {
+		return name;
+	}
+
+	void setPassword() {
+		this->password = password;
+	}
+
+	string getPassword() {
+		return password;
+	}
 
 };
