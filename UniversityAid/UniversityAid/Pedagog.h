41e895aff5732bd46098e0ca891e0f946a01a47d
Media <Media@DESKTOP-GTH07HE>
2018-12-17 19:33:45 +0100

    shife njeher ket header pedagog

--- UniversityAid/UniversityAid/Pedagog.h
+++ UniversityAid/UniversityAid/Pedagog.h
@@ -1,6 +1,36 @@
 #pragma once
 using namespace std;
 class Pedagog {
+	class Pedagog :public Person {
+	private:
+		string ID;
+		string password;
+		int paga;
+    public:
+		Pedagog() :Person() {}
+		Pedagog(string name, string surname) :Person(name, surname) {}
+		string showName() {
+			return name;
+		}
+		void setID() {
+			this->ID = ID;
+		}
+		string getID() {
+			return ID;
+		}
 
+		void setPassword() {
+			this->password = password;
+		}
 
+		string getPassword() {
+			return password;
+		}
+		void setPaga() {
+			this->paga = paga;
+		}
+		 
+		int getPaga() {
+			return paga;
+		}
 };
