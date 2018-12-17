634f5d80a7e12262f76fbd2e3328905bcd53dd45
Media <Media@DESKTOP-GTH07HE>
2018-12-17 22:06:16 +0100

    shtova dep

--- UniversityAid/UniversityAid/Departament.h
+++ UniversityAid/UniversityAid/Departament.h
@@ -1,6 +1,31 @@
 #pragma once
 using namespace std;
+#include "Libraries.h"
 class Departament {
+protected:
 
+	string clubs;
+	
+public:
+	int classes;
+	int labs;
+	int buildings;
+
+	Departament() {
+		classes = 0;
+		labs = 0;
+		buildings = 0;
+	}
+	Departament(int classes, int labs, int buildings) {
+		this->classes = classes;
+		this->labs = labs;
+		this->buildings = buildings;
+	}
+	void setClubs(string clubs) {
+		this->clubs = clubs;
+	}
+	string getClubs() {
+		return clubs; 
+	}
 
 };
