0343859effa83cccd333a39d95bdbea1d46bc747
Media <Media@DESKTOP-GTH07HE>
2019-01-03 01:43:15 +0100

    shtova lendet te departamenti, shikoje njehere ca kam bere se nuk jam e sigurt.

--- UniversityAid/UniversityAid/Departament.h
+++ UniversityAid/UniversityAid/Departament.h
@@ -1,20 +1,26 @@
 #pragma once
 using namespace std;
 #include "Faculty.h"
+#include "Subjects.h"//pse e ke bere include faculty? a nuk na duhet se nuk po e
+//perdorim me trazhgimin? 
+//ktu tani duhet te krijojme programin e studimit
+//si vektor, edhe studentet 
 class Departament {
-private:
+protected:
 	int id;
 public:
 	string nameDepartament;
+	vector<Subjects> lendet;
 	Departament()
 	{
 		id = 0;
 		nameDepartament = "Unknown";
 	}
-	Departament(int id, string name)
+	Departament( int id, string name, vector<Subjects> lendet)
 	{
 		this->id = id;
 		this->nameDepartament = name;
+		this->lendet = lendet; 
 	}
 	int getId()
 	{
@@ -26,6 +32,21 @@ public:
 	}
 
 };
+void checkDepartments(vector<Departament> departamentet) {
+	for (int i = 0; i < departamentet.size(); i++)
+	{
+		cout << "Te dhenat e departamentit " << i + 1 << endl;
+		cout << departamentet[i].nameDepartament << endl;
+		cout << "Lendet jane: " << endl;
+		for (int j = 0; j < departamentet[i].lendet.size(); i++)
+		{
+			cout << "Emer: " << departamentet[i].lendet[j].name << endl;
+			cout << "Oret: " << departamentet[i].lendet[j].getHours() << endl;
+			cout << "Kreditet: " << departamentet[i].lendet[j].getCredits() << endl;
+		}
+	}
+};
+
 
 //leftover code
 
